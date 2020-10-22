e3dbd5e9ad8c4f7624b8b24535c031d5b3050cac
shoma <ts1988220@gmail.com>
2020-10-22 10:49:00 +0900

    create_top

--- application/controllers/Nanpa.php
+++ application/controllers/Nanpa.php
@@ -14,21 +14,24 @@ class Nanpa extends CI_Controller
         $this->main();
     }
     public function main(){
-        $this->load->view('top_view.php');
+        $this->load->view('top_view');
     }
     public function serch(){
-        $this->load->view('serch_view.php');
+        $this->load->view('serch_view');
     }
     public function profile(){
-        $this->load->view('profile_view.php');
+        $this->load->view('profile_view');
     }
     public function twit(){
-        $this->load->view('twit_view.php');
+        $this->load->view('twit_view');
     }
     public function json(){
         $config_file ="../js/pref_city.json";
         $config_json = file_get_contents($config_file);
         $elements = json_decode($config_json, true);
+    }
+    public function test(){
+        $this->load->view('main_view');
     }
 }
 ?>
