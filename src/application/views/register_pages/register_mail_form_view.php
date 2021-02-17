832839993ba93e4ca8c9af884d17c18d6aec3cc1
shoma <ts1988220@gmail.com>
2021-02-17 20:10:49 +0900

    validationのチェック（新規登録ページ）

--- src/application/views/register_pages/register_mail_form_view.php
+++ src/application/views/register_pages/register_mail_form_view.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
-<html>
+<html lang="ja">
 
 <head>
 	<title>会員登録画面</title>
 	<meta charset="utf-8">
+	<link rel="stylesheet" href="<?php echo base_url()."assets/css/login.css";?>">
 </head>
 
 <body>
@@ -12,22 +13,22 @@
 
 		<h5>メールアドレス(ログインID)</h5>
 		<?php echo form_error('email'); ?>
-		<input type="email" name="email" value="<?php echo set_value('email'); ?>" size="30">
+		<input type="email" name="email" value="<?php echo set_value('email'); ?>" size="30" />
 		<h5>パスワード</h5>
 		<?php echo form_error('password'); ?>
-		<input type="password" name="password" value="<?php echo set_value('email'); ?>" size="30">
+		<input type="password" name="password" value="<?php echo set_value('password'); ?>" size="30" />
 		<h5>パスワード確認</h5>
 		<?php echo form_error('cpassword'); ?>
-		<input type="password" name="cpassword" size="30"></p>
+		<input type="password" name="cpassword"value="<?php echo set_value('cpassword'); ?>" size="30" /></p>
 		<div>
 			<?php echo form_error('age_submit'); ?>
-			<input type="checkbox" name="age_submit" value="<?php echo set_value('age_submit'); ?>">
+			<input type="checkbox" name="age_submit" value="1"<?php echo set_checkbox('age_submit','1'); ?> />
 			<label for="">私は18歳以上です。</label>
-			<p style="color:red; font-size:2vh;"> ※18歳未満の方はご利用できません。</p>
+			<p style="font-size:1vh;"> ※18歳未満の方はご利用できません。</p>
 		</div>
 		<div>
 			<?php echo form_error('terms'); ?>
-			<input type="checkbox" name="terms" value="<?php echo set_value('terms'); ?>">
+			<input type="checkbox" name="terms" value="1" <?php echo set_checkbox('terms','1'); ?> />
 			<label for="terms">利用規約に同意する。</label><br>
 		</div>
 		<br>
