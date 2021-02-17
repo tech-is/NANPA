21746a3f05f49e3a4085594b640151d2352fceb2
shoma <ts1988220@gmail.com>
2021-02-17 14:46:37 +0900

    言語対応

--- src/application/config/config.php
+++ src/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://compass.com/';
+$config['base_url'] = 'http://compass_tech.com/';
 
 /*
 |--------------------------------------------------------------------------
@@ -76,7 +76,7 @@ $config['url_suffix'] = '';
 | than english.
 |
 */
-$config['language']    = 'english';
+$config['language']    = 'japanese';
 
 /*
 |--------------------------------------------------------------------------
--- src/application/controllers/Compass.php
+++ src/application/controllers/Compass.php
@@ -9,7 +9,7 @@ class Compass extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        $this->load->helper(array('form', 'url'));
+        $this->load->helper(array('url','form'));
         $this->load->model("Compass_model");
     }
     public function index()
@@ -52,10 +52,12 @@ class Compass extends CI_Controller
         }
 
     }
-    public function profile(){
+    public function profile()
+    {
         $this->load->view('profile_pagas/profile_view');
     }
-    public function twit(){
+    public function twit()
+    {
         $this->load->view('profile_pagas/twit_view');
     }
     // public function json(){
@@ -63,16 +65,48 @@ class Compass extends CI_Controller
     //     $config_json = file_get_contents($config_file);
     //     $elements = json_decode($config_json, true);
     // }
-    // 登録（仮）フォームへ
-    public function register_mail_form(){
+
+    // 新規登録
+    public function register_mail_form()
+    {
         $this->load->view('register_pages/register_mail_form_view');
     }
+    public function register_check()
+    {
+
+        $data = array(
+            "email" => $this->input->post('email'),
+            "password" => $this->input->post('password'),
+            "cpassword" => $this->input->post('cpassword'),
+            "age_submit" => $this->input->post('age_submit'),
+            "terms" => $this->input->post('terms')
+        );
+
+        $this->load->library('form_validation');
+        
+        $this->form_validation->set_rules('email', 'メールアドレス', 'required|valid_email|xss_clean');
+        $this->form_validation->set_rules('password', 'パスワード', 'required|');
+        $this->form_validation->set_rules('cpassword', 'パスワードの確認', 'required|matches[password]');
+        $this->form_validation->set_rules('age_submit', '年齢確認', 'required');
+        $this->form_validation->set_rules('terms', '利用同意', 'required');
+
+        if ($this->form_validation->run() == FALSE){
+
+            $this->load->view('register_pages/register_mail_form_view');
+        } else {
+            $this->Compass_model->add_users($data);
+        }
+
+
+    }
     // 登録（本）フォームへ
-    public function register_form(){
+    public function register_form()
+    {
         $this->load->view('register_pages/register_form_view');
     }
     //ログインフォームへ
-    public function login(){
+    public function login()
+    {
         $this->load->view('login_pages/login_form_view');
     }
 }
