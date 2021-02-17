847c8cdd02aadd7c96f59f1f96d68d92b3b19b37
shoma <ts1988220@gmail.com>
2021-02-18 06:38:53 +0900

    モデルのインサート文を作成しました。

---
+++ src/application/views/register_pages/register_done_view.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>会員登録完了画面</title>
+	<meta charset="utf-8">
+</head>
+
+<body>
+		<h1><?php echo $email;?>さん登録完了しました！</h1>
+		<p>登録が完了しました。<br>ログインページからログインしてご利用ください。</p>
+		<p><a href="/compass/login">ログイン画面に戻る</a></p>
+
+</body>
+
+</html>
