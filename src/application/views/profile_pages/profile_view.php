0971a70a63a0fdced984d2a39284a631b77ad044
shoma <ts1988220@gmail.com>
2021-03-13 16:17:18 +0900

    button_edt

--- src/application/views/profile_pages/profile_view.php
+++ src/application/views/profile_pages/profile_view.php
@@ -4,11 +4,12 @@
 <head>
    <meta charset="utl-8">
    <title>マイページ</title>
+   <link rel="stylesheet" href="<?php echo base_url()."assets/css/style.css"; ?>">
 </head>
 
 <body>
    <?php foreach($results as $result):?>
-   <h1>マイページ</h1>
+   <h2>マイページ</h2>
    <label for="name">メールアドレス</label>
    <p><?php 
          if(isset($result['email'])){
@@ -54,9 +55,15 @@
             echo "プロフィール写真が登録されていません。";
          }; ?>
    </p>
+
+   <?php echo form_open('compass/select_pages'); ?>
+      <input type="hidden" name="user_id" value="<?php echo $result['id']; ?>">
+      <input id="radio_1" class="radio_button" type="radio" name="pages" value="1" /><label for="radio_1">登録内容を変更する</label>
+      <input id="radio_2" class="radio_button" type="radio" name="pages" value="2" /><label for="radio_2">相手を探す</label>
+      <input id="radio_3" class="radio_button" type="radio" name="pages" value="3" /><label for="radio_3">ひとこと投稿</label>
+      <p><input type="submit" value="送信する"></p>
+   </form>
    <?php endforeach; ?>
-   <a class="botton" href="<?php echo base_url()."compass/profile_change";?>">登録内容を変更する</a>
-   <a class="botton" href="<?php echo base_url()."compass/top";?>">TOPに戻る</a>
 </body>
 
 </html>
